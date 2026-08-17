5b6da3c9ec695d5ce1a99b69f9e9361dc10c27c1
SocieteG1234 <bianchilauraine@gmail.com>
2026-08-17 19:20:04 +0000

    cdfgg

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,7 +3,7 @@
 
 const DEV_MODE = true;
 const STORAGE_KEY = 'bnp_users_data';
-const DATA_VERSION = 4
+const DATA_VERSION = 6
  ; // ⚡ INCRÉMENTER CE NUMÉRO À CHAQUE MODIFICATION
 
 class UserService {
